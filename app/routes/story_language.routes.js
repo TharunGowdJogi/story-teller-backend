901f723cc101a98c07741dce9b9fbdc8268affdd
rajashekarreddy99 <rajashekar.punnam@eagles.oc.edu>
2024-06-10 16:27:55 -0400

    Added routes for story languages

--- app/routes/story_language.routes.js
+++ app/routes/story_language.routes.js
@@ -1,8 +1,12 @@
 module.exports = (app) => {
-    const { getAllLanguages } = require("../controllers/story_language.controller.js");
-    var languageRouter = require("express").Router();
-  
-    languageRouter.get("/languages/", getAllLanguages);
-    app.use("/", languageRouter);
-  };
-  
+  const { createLanguage, getAllLanguages, getLanguageById, updateLanguage, deleteLanguage, deleteAllLanguages } = require("../controllers/story_language.controller.js");
+  var languageRouter = require("express").Router();
+
+  languageRouter.post("/languages/", createLanguage);
+  languageRouter.get("/languages/", getAllLanguages);
+  languageRouter.get("/languages/:id", getLanguageById);
+  languageRouter.put("/languages/:id", updateLanguage);
+  languageRouter.delete("/languages/:id", deleteLanguage);
+  languageRouter.delete("/languages/", deleteAllLanguages);
+  app.use("/", languageRouter);
+};

d1f4248323813560072928d125f6aac049465e34
rajashekarreddy99 <rajashekar.punnam@eagles.oc.edu>
2024-06-10 22:06:56 -0400

    Raja roles for story (#14)
    * Added controllers for story roles
    
    * Added routes for story roles

--- app/routes/story_role.routes.js
+++ app/routes/story_role.routes.js
@@ -1,8 +1,12 @@
 module.exports = (app) => {
-    const {getAllRoles } = require("../controllers/story_role.controller.js");
-    var storyRoleRouter = require("express").Router();
-  
-    storyRoleRouter.get("/roles/", getAllRoles);
-    app.use("/", storyRoleRouter);
-  };
-  
+  const { createRole, getAllRoles, getRoleById, updateRole, deleteRole, deleteAllRoles } = require("../controllers/story_role.controller.js");
+  var storyRoleRouter = require("express").Router();
+
+  storyRoleRouter.post("/roles/", createRole);
+  storyRoleRouter.get("/roles/", getAllRoles);
+  storyRoleRouter.get("/roles/:id", getRoleById);
+  storyRoleRouter.put("/roles/:id", updateRole);
+  storyRoleRouter.delete("/roles/:id", deleteRole);
+  storyRoleRouter.delete("/roles/", deleteAllRoles);
+  app.use("/", storyRoleRouter);
+};
